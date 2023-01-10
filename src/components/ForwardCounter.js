b44997662b13cd67310890e5bc0a58f88331a201
Rian Yuri <rianyurirgt@gmail.com>
2023-01-10 18:51:48 -0300

    Learning custom hooks

---
+++ src/components/ForwardCounter.js
@@ -0,0 +1,10 @@
+import Card from './Card';
+import useCounter from '../hooks/use-counter';
+const ForwardCounter = () => {
+
+useCounter();
+const counter = useCounter();
+ return <Card>{counter}</Card>;
+};
+
+export default ForwardCounter;
